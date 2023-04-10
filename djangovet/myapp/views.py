8d12cf33765bb517246a71ca665b64ec35833aff
sigristarisa <arisasigrist.ch@gmail.com>
2023-04-10 15:33:15 +0200

    added templates directory and edited views.py

--- djangovet/myapp/views.py
+++ djangovet/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+def home():
+  template = loader.get_template("app/home.html")
+  return HttpResponse(template.render())
