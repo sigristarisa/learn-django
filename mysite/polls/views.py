4a5640f95abf54fedb9bf564e60848bb5460518c
sigristarisa <arisasigrist.ch@gmail.com>
2023-06-01 17:40:13 +0200

    created poll template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return render("hello world!")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    return render(request, "You're looking at question %s." % question_id)
+
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s."
+    return render(request, response % question_id)
+
+
+def vote(request, question_id):
+    return render(request, "You're voting on question %s." % question_id)
