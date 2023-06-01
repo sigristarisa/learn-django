13908b5c43207bf4bc75e44b62e7474088194c3a
sigristarisa <arisasigrist.ch@gmail.com>
2023-06-01 14:41:16 +0200

    removed all files

--- djangovet/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-
-# Create your views here.
-def home():
-  template = loader.get_template("myapp/home.html")
-  return HttpResponse(template.render())
